bf928731a60b3b5e9c130bd6eabc0fb688377a8f
jangbohyun <jangbohyun@192.168.0.13>
2017-11-04 02:10:15 +0900

    17.11.04 boj 3062 미해결...ㅠㅠ 왜????

--- src/com/algorithms/BOJ/Solution_3062.java
+++ src/com/algorithms/BOJ/Solution_3062.java
@@ -61,43 +61,68 @@ public class Solution_3062 {
 		return result;
 	}
 
+	// 비대칭 배열
+	static int[] arrRevers(int[] arr) {
+
+		int temp = arr.length - 1;
+		int[] result = new int[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+
+			result[i] = arr[temp];
+			temp--;
+		}
+
+		return result;
+	}
+
 	static String sameNumber(int N) {
 
 		String result = null;
 		int numberSize1 = numberSize(N);
-		int[] arr = new int[numberSize1];
-		int sumN = 0;
+		int[] arr = new int[numberSize1]; // 원래 입력 수.
+		int[] arrR = new int[numberSize1]; // 입력 수 뒤집은 수 .
+		int sum = 0;
 
 		// 입력된 수를 자리수에 맞춰 배열에 저장.
 		arr = arrNum(N);
 
-		for (int i = 0; i < arr.length; i++)
-			sumN += arr[i];
+		// 입력 된 수를 뒤집어서 다른 배열에 저장.
+		arrR = arrRevers(arr);
 
-		//System.out.println("SUM:: " + sumN);
+		int NR = 0;
+		int arrRSize = arrR.length - 1;
+		for (int i = 0; i < arrR.length; i++) {
+			NR += arrR[i] * Math.pow(10, arrRSize);
+			arrRSize--;
+		}
+
+		// 입력된 수, 뒤집은 수를 더하기.
+		sum = NR + N;
 
 		// 총합의 좌우 대칭 비교.
-		int numberSize2 = numberSize(sumN);
+		int numberSize2 = numberSize(sum);
 		int[] arrSum = new int[numberSize2];
-		arrSum = arrNum(sumN);
+		// 비교하기 위해 배열에 저장.
+		arrSum = arrNum(sum);
 
 		// 배열의 첫번째항과 마지막항의 값이 같은지 비교.
 		int divdeNumber = numberSize2 - 1;
 		String compareResult = null;
-		if (numberSize2 == 1) {
+		if (numberSize2 <= 2) {
 			result = "YES";
-		}
-		for (int i = 0; i < numberSize2 / 2; i++) {
+		} else {
+			for (int i = 0; i < numberSize2 / 2; i++) {
 
-			compareResult = compareNumber(arrSum[i], arrSum[divdeNumber]);
-			if (compareResult == "NO") {
-				result = compareResult;
-				break;
-			} else {
-				result = compareResult;
-				divdeNumber--;
+				compareResult = compareNumber(arrSum[i], arrSum[divdeNumber]);
+				if (compareResult == "NO") {
+					result = compareResult;
+					break;
+				} else {
+					result = compareResult;
+					divdeNumber--;
+				}
+
 			}
-
 		}
 
 		return result;
@@ -113,10 +138,11 @@ public class Solution_3062 {
 		for (int i = 0; i < testCase; i++) {
 			n = sc.nextInt();
 			result[i] = sameNumber(n);
-			//System.out.println(result);
+			// System.out.println(result);
 		}
-		
-		for(int i=0; i<testCase; i++) System.out.println(result[i]);
+
+		for (int i = 0; i < testCase; i++)
+			System.out.println(result[i]);
 
 	}
 
