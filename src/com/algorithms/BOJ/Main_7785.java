fb595a1a899810fd909e745916389d9a75f5e9a7
jangbohyun <jangbohyun@192.168.0.2>
2017-11-14 15:22:18 +0900

    2017.11.14

--- src/com/algorithms/BOJ/Main_7785.java
+++ src/com/algorithms/BOJ/Main_7785.java
@@ -1,6 +1,6 @@
 package com.algorithms.BOJ;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main_7785 {
 
@@ -8,13 +8,25 @@ public class Main_7785 {
 	 * 17.11.12 boj 7785) 상근이는 세계적인 소프트웨어 회사 기글에서 일한다. 이 회사의 가장 큰 특징은 자유로운 출퇴근 시간이다.
 	 * 따라서, 직원들은 반드시 9시부터 6시까지 회사에 있지 않아도 된다. 각 직원은 자기가 원할 때 출근할 수 있고, 아무때나 퇴근할 수
 	 * 있다. 상근이는 모든 사람의 출입카드 시스템의 로그를 가지고 있다. 이 로그는 어떤 사람이 회사에 들어왔는지, 나갔는지가 기록되어져 있다.
-	 * 로그가 주어졌을 때, 현재 회사에 있는 모든 사람을 구하는 프로그램을 작성하시오.
+	 * 로그가 주어졌을 때, 현재 회사에 있는 모든 사람을 구하는 프로그램을 작성하시오. input) 4 Baha enter Askar enter
+	 * Baha leave Artem enter
 	 */
-	static void commute(String[][] s, int n) {
+	static void commute(Map<String, String> s) {
 
-		String[] result = new String[n];
+		ArrayList<String> result = new ArrayList<>();
 
+		for (Map.Entry<String, String> entry : s.entrySet()) {
+
+			if (entry.getValue().equals("enter")) {
+				result.add(entry.getKey());
+			} // if
+
+		} // for
 		
+		Collections.sort(result);
+		
+		for(int i=result.size()-1; i>=0; i--) System.out.println(result.get(i));
+
 	}
 
 	public static void main(String[] args) {
@@ -22,16 +34,16 @@ public class Main_7785 {
 		Scanner sc = new Scanner(System.in);
 
 		int n = sc.nextInt();
-		String[][] s = new String[n][2];
+		Map<String, String> s = new HashMap<String, String>();
 
 		for (int i = 0; i < n; i++) {
-
-			s[i][0] = sc.next();
-			s[i][1] = sc.next();
+			String name = sc.next();
+			String state = sc.next();
+			s.put(name, state);
 
 		}
 
-		commute(s, n);
+		commute(s);
 
 		sc.close();
 
